02296416ae4a996ef05ee2b9ed2587edd06ac043
cesar augusto gadelha <cesar_ags@outlook.com>
2025-02-22 09:30:18 -0300

    Enhance resilience and update project configuration
    Reordered using directives in ServiceCollectionExtensions.cs.
    Updated AddDbContext method calls to include EnableRetryOnFailure.
    Incremented version number in HybridRepoNet.csproj from 3.0.7 to 3.0.8.
    Changed icone-repo.png path to a relative path in HybridRepoNet.csproj.
    Added Microsoft.Extensions.Http.Polly and Polly package references.
    Introduced ResiliencePolicies class with retry and circuit breaker policies.

--- src/HybridRepoNet/Configurations/ServiceCollectionExtensions.cs
+++ src/HybridRepoNet/Configurations/ServiceCollectionExtensions.cs
@@ -1,9 +1,9 @@
-﻿using MediatR;
+﻿using HybridRepoNet.Abstractions;
+using HybridRepoNet.Repository;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using HybridRepoNet.Abstractions;
-using HybridRepoNet.Repository;
 
 namespace HybridRepoNet.Configurations;
 
@@ -15,12 +15,12 @@ public static class ServiceCollectionExtensions
         if (provider.Equals("PostgreSQL", StringComparison.OrdinalIgnoreCase))
         {
             services.AddDbContext<TContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+                options.UseNpgsql(configuration.GetConnectionString("PostgresConnection"), npgsqlOptions => npgsqlOptions.EnableRetryOnFailure(5)));
         }
         else if (provider.Equals("SQLServer", StringComparison.OrdinalIgnoreCase))
         {
             services.AddDbContext<TContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("SqlConnection")));
+                options.UseSqlServer(configuration.GetConnectionString("SqlConnection"), sqlOptions => sqlOptions.EnableRetryOnFailure(5)));
         }
         else
         {
